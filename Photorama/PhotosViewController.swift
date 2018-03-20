e2fc0703ecf11102630c2597abf027d823c816e5
Sergey <sharipov14@gmail.com>
2018-03-19 20:47:50 -0400

    Added collection view.

--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -8,47 +8,53 @@
 
 import UIKit
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, UICollectionViewDelegate {
     
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     var store = PhotoStore()
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    let photoDataSource = PhotoDataSource()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.dataSource = photoDataSource
+        collectionView.delegate = self
         
         store.fetchInterestingPhotos {
             (photosResult) -> Void in
             
             switch photosResult {
             case let .success(photos):
-                print("Successfully found \(photos.count) photos")
-                if let firstPhoto = photos.first {
-                    self.updateImageView(for: firstPhoto)
-                }
+                print("Successfully found \(photos.count) photos.")
+                self.photoDataSource.photos = photos
             case let .failure(error):
                 print("Error fetching recent photos: \(error)")
+                self.photoDataSource.photos.removeAll()
             }
-            
-            
+            self.collectionView.reloadSections(IndexSet(integer: 0))
         }
     }
     
-    func updateImageView(for photo: Photo) {
-        self.store.fetchImage(for: photo) {
-            (imageResult) -> Void in
-            switch imageResult {
-            case let .success(image):
-                OperationQueue.main.addOperation {
-                    self.imageView.image = image
-                }
-            case let .failure(error):
-                print("Error downloading image: \(error)")
+    func collectionView(_ collectionView: UICollectionView,
+                        willDisplay cell: UICollectionViewCell,
+                        forItemAt indexPath: IndexPath) {
+        
+        let photo = photoDataSource.photos[indexPath.row]
+
+        store.fetchImage(for: photo, completion: { (result) -> Void in
+            
+            guard let photoIndex = self.photoDataSource.photos.index(of: photo),
+                case let .success(image) = result else {
+                    return
             }
-        }
+            let photoIndexPath = IndexPath(item: photoIndex, section: 0)
+      
+            if let cell = self.collectionView.cellForItem(at: photoIndexPath)
+                as? PhotoCollectionViewCell {
+                cell.update(with: image)
+            }
+        })
     }
+
 }
